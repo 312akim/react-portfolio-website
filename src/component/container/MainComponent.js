a59d132b841736457dbe0343a767800001c8a6b4
Alex Kim <312akim@gmail.com>
2021-01-29 20:33:32 -0800

    Project init

---
+++ src/component/container/MainComponent.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const MainComponent = () => {
+    return (
+        <div>
+            This is the main page
+        </div>
+    )
+}
+
+export default MainComponent;
