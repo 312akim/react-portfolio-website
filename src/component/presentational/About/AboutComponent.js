cee06830efb09d6e60ef36ff1526fe0f3acbf8d6
Alex Kim <312akim@gmail.com>
2021-03-11 12:00:06 -0800

    Set about component cube sizing & text on faces

--- src/component/presentational/About/AboutComponent.js
+++ src/component/presentational/About/AboutComponent.js
@@ -1,9 +1,64 @@
 import React from 'react';
 import COLORS from '../../../shared/design/colorTheme';
 import { StyledSectionComponent, StyledSectionHeader, StyledSectionSubheader, StyledSectionText  } from '../../../shared/sharedComponents/SectionComponentStyles';
-import { StyledAboutHero, StyledAboutSubsection } from './AboutComponentStyles';
+import { StyledAboutHero, StyledAboutCubeTextContainer, StyledCubeContainer, StyledCube, StyledCubeFace } from './AboutComponentStyles';
 
 export const AboutComponent = () => {
+
+    const CubeComponent = () => {
+        return (
+            <StyledCubeContainer>
+                <StyledCube>
+                    <StyledCubeFace transform='rotateY(0deg)' backgroundColor='red'>
+                        <StyledSectionSubheader>
+                            Entrepreneur at heart
+                        </StyledSectionSubheader>
+                        <StyledSectionText>
+                            Growing up with parents who were small business owners, business has always been part of my life.
+                            I've had the unique opportunity to see how product, service and interaction work together to uplift a business
+                            beyond all expectations or bury it's future. Consequently i'm always thinking of opportunities and keeping an
+                            eye out for improvements.
+                        </StyledSectionText>
+                    </StyledCubeFace>
+                    <StyledCubeFace transform='rotateY(-90deg)' backgroundColor='yellow'>
+                        <StyledAboutCubeTextContainer height={'41vh'}>
+                            <StyledSectionSubheader>
+                                Why I love coding
+                            </StyledSectionSubheader>
+                            <StyledSectionText>
+                                There are 2 reasons I truly love coding.
+                            </StyledSectionText>
+                            <StyledSectionText>
+                                1. It lets me to turn my ideas into practice
+                            </StyledSectionText>
+                            <StyledSectionText>
+                                Sample Text
+                            </StyledSectionText>
+                            <StyledSectionText>
+                                2. It's a creative outlet that doesn't require talent
+                            </StyledSectionText>
+                            <StyledSectionText>
+                                Never before have I experienced such a natural and easy way to express my own creativity. Where I normally struggle
+                                in arts or music, coding lets me express creativity in my own way.
+                            </StyledSectionText>
+                        </StyledAboutCubeTextContainer>
+                    </StyledCubeFace>
+                    <StyledCubeFace transform='rotateX(-90deg)' backgroundColor='blue'>
+                        <StyledAboutCubeTextContainer height={'17vh'}>
+                            <StyledSectionSubheader>
+                                What I want to achieve
+                            </StyledSectionSubheader>
+                            <StyledSectionText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+                                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+                            </StyledSectionText>
+                        </StyledAboutCubeTextContainer>
+                    </StyledCubeFace>
+                </StyledCube>
+            </StyledCubeContainer>
+        )
+    }
+
     return (
         <StyledSectionComponent backgroundColor={COLORS.primaryLight}>
             <StyledSectionHeader>
@@ -11,52 +66,7 @@ export const AboutComponent = () => {
             </StyledSectionHeader>
             <StyledAboutHero src=''/>
 
-            <StyledAboutSubsection height={'19vh'}>
-                <StyledSectionSubheader>
-                    Entrepreneur at heart
-                </StyledSectionSubheader>
-                <StyledSectionText>
-                    Growing up with parents who were small business owners, business has always been part of my life.
-                    I've had the unique opportunity to see how product, service and interaction work together to uplift a business
-                    beyond all expectations or bury it's future. Consequently i'm always thinking of opportunities and keeping an
-                    eye out for improvements.
-                </StyledSectionText>
-            </StyledAboutSubsection>
-
-            <StyledAboutSubsection height={'41vh'}>
-                <StyledSectionSubheader>
-                    Why I love coding
-                </StyledSectionSubheader>
-                <StyledSectionText>
-                    There are 2 reasons I truly love coding.
-                </StyledSectionText>
-                <StyledSectionText>
-                    1. It lets me to turn my ideas into practice
-                </StyledSectionText>
-                <StyledSectionText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-                    nulla pariatur.
-                </StyledSectionText>
-                <StyledSectionText>
-                    2. It's a creative outlet that doesn't require talent
-                </StyledSectionText>
-                <StyledSectionText>
-                    Never before have I experienced such a natural and easy way to express my own creativity. Where I normally struggle
-                    in arts or music, coding lets me express creativity in my own way.
-                </StyledSectionText>
-            </StyledAboutSubsection>
-
-            <StyledAboutSubsection height={'17vh'}>
-                <StyledSectionSubheader>
-                    What I want to achieve
-                </StyledSectionSubheader>
-                <StyledSectionText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-                </StyledSectionText>
-            </StyledAboutSubsection>
+            <CubeComponent />
         </StyledSectionComponent>
     )
 }
--- src/component/presentational/About/AboutComponentStyles.js
+++ src/component/presentational/About/AboutComponentStyles.js
@@ -1,6 +1,19 @@
 import COLORS from '../../../shared/design/colorTheme';
-import styles from 'styled-components';
+import styles, {keyframes} from 'styled-components';
 
+
+    // Animations
+const cubeAnimation = keyframes`
+    0% {
+
+    }
+    100% {
+
+    }
+`
+
+
+    // Components
 // 12vh/100
 export const StyledAboutHero = styles.img`
     width: 80%;
@@ -8,8 +21,34 @@ export const StyledAboutHero = styles.img`
 `
 
 // margin-bot 9vh total
-export const StyledAboutSubsection = styles.div`
+export const StyledAboutCubeTextContainer = styles.div`
     height: ${props => props.height};
     margin-bottom: 3vh;
-    background: yellow;
+`
+
+//Scene
+export const StyledCubeContainer = styles.div`
+    width: 100%;
+    height: 60vh;
+    perspective: 10000px;
+    margin-left: 20%;
+    margin-top: 30%;
+`
+//Cube
+export const StyledCube = styles.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    transform-style: preserve-3d;
+    transform: translateZ(-100px);
+    transform: rotateX(45deg) rotateY(45deg);
+`
+
+export const StyledCubeFace = styles.div`
+    position: absolute;
+    width: 30vw;
+    height: 30vw;
+    opacity: 0.5;
+    transform: ${props => props.transform} translateZ(15vw);
+    background: ${props => props.backgroundColor};
 `
