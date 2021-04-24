4b46fe418ea3e16062e6eded131c0a65aa5585b9
Alex Kim <312akim@gmail.com>
2021-04-24 04:43:01 -0700

    Setup initial github api commit data

--- src/component/presentational/Activity/ActivityComponent.js
+++ src/component/presentational/Activity/ActivityComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import COLORS from '../../../shared/design/colorTheme';
 import { StyledSectionComponent, StyledSectionHeader } from '../../../shared/sharedComponents/SectionComponentStyles';
+import { Octokit } from "@octokit/core";
 
 export const ActivityComponent = () => {
     return (
@@ -8,6 +9,50 @@ export const ActivityComponent = () => {
             <StyledSectionHeader>
                 Recent Activity
             </StyledSectionHeader>
+            <div>
+                <CommitListComponent />
+            </div>
         </StyledSectionComponent>
     )
+}
+
+//Github API Latest 5 commits to a repo
+const CommitListComponent = () => {
+  const [commits, setCommits] = useState(false);
+
+  const octokit = new Octokit({ 
+    auth: process.env.GIT_TOKEN 
+  });
+
+  useEffect(() => {
+    const owner = '312akim',
+          repo = 'apparel-store-mern',
+          perPage = 5;
+    
+
+    const fetchData = async () => {
+        const fiveMostRecentCommits = await octokit.request(
+            `GET /repos/{owner}/{repo}/commits`, { owner, repo, per_page: perPage }
+        );
+
+        setCommits(fiveMostRecentCommits);
+        console.log("Commits: " + JSON.stringify(commits));
+    }
+
+    fetchData();
+    }, [])
+
+    return (
+      <>
+        <ul>
+          {!commits ? <div>failed to load data</div> :
+            commits.data.map(commit => (
+              <li key={commit.id}>
+                  {commit.commit.author.name}: {commit.commit.message}
+                  <a href={commit.html_url}> Link</a>
+              </li>
+          ))}
+        </ul>
+      </>
+    );
 }
