841efd6f4fc0119cdcc979c4a380770296d4900b
Alex Kim <312akim@gmail.com>
2021-04-24 18:45:08 -0700

    Added Portfolio site commits to activity section

--- src/component/presentational/Activity/ActivityComponent.js
+++ src/component/presentational/Activity/ActivityComponent.js
@@ -18,7 +18,8 @@ export const ActivityComponent = () => {
 
 //Github API Latest 5 commits to a repo
 const CommitListComponent = () => {
-  const [commits, setCommits] = useState(false);
+  const [apparelStoreCommits, setApparelStoreCommits] = useState(false);
+  const [portfolioCommits, setPortfolioCommits] = useState(false);
 
   const octokit = new Octokit({ 
     auth: process.env.GIT_TOKEN 
@@ -26,29 +27,49 @@ const CommitListComponent = () => {
 
   useEffect(() => {
     const owner = '312akim',
-          repo = 'apparel-store-mern',
+          repo1 = 'apparel-store-mern',
+          repo2 = 'react-portfolio-website',
           perPage = 5;
     
 
-    const fetchData = async () => {
-        const fiveMostRecentCommits = await octokit.request(
-            `GET /repos/{owner}/{repo}/commits`, { owner, repo, per_page: perPage }
+    const fetchApparelStoreData = async () => {
+        const fiveMostRecentApparelCommits = await octokit.request(
+            `GET /repos/{owner}/{repo1}/commits`, { owner, repo1, per_page: perPage }
         );
 
-        setCommits(fiveMostRecentCommits);
-        console.log("Commits: " + JSON.stringify(commits));
+        setApparelStoreCommits(fiveMostRecentApparelCommits);
     }
 
-    fetchData();
+    const fetchPortfolioCommits = async () => {
+        const fiveMostRecentPortfolioCommits = await octokit.request(
+            `GET /repos/{owner}/{repo2}/commits`, { owner, repo2, per_page: perPage }
+        );
+
+        setPortfolioCommits(fiveMostRecentPortfolioCommits);
+    }
+
+    fetchApparelStoreData();
+    fetchPortfolioCommits();
     }, [])
 
     return (
       <>
         <ul>
-          {!commits ? <div>failed to load data</div> :
-            commits.data.map(commit => (
+          <h2>Apparel Store Commits</h2>
+          {!apparelStoreCommits ? <div>failed to load commit data</div> :
+            apparelStoreCommits.data.map(commit => (
               <li key={commit.id}>
-                  {commit.commit.author.name}: {commit.commit.message}
+                  {new Date(commit.commit.author.date).toLocaleDateString()}: {commit.commit.message}
+                  <a href={commit.html_url}> Link</a>
+              </li>
+          ))}
+        </ul>
+        <ul>
+          <h2>Portfolio Site Commits</h2>
+          {!portfolioCommits ? <div>failed to load commit data</div> :
+            portfolioCommits.data.map(commit => (
+              <li key={commit.id}>
+                  {new Date(commit.commit.author.date).toLocaleDateString()}: {commit.commit.message}
                   <a href={commit.html_url}> Link</a>
               </li>
           ))}
