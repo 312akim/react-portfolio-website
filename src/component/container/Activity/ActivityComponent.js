3d94283e031648f494ced1b92bf9cfa41e60b4c9
Alex Kim <312akim@gmail.com>
2021-06-05 01:51:42 -0700

    Set activity component to show 5 commits, removed commit # reduction based on screen height

--- src/component/container/Activity/ActivityComponent.js
+++ src/component/container/Activity/ActivityComponent.js
@@ -31,8 +31,11 @@ const CommitListComponent = () => {
     const owner = '312akim',
           repo1 = 'apparel-store-mern',
           repo2 = 'react-portfolio-website',
+          // **********
           // Show 3 commits if smaller vertical sized screen
-          perPage = window.document.body.offsetHeight < 4300 ? 3 : 4;
+          // perPage = window.document.body.offsetHeight < 4300 ? 3 : 4;
+          // **********
+          perPage = 5;
     
     console.log("Height: " + window.document.body.offsetHeight);
     const fetchApparelStoreData = async () => {
